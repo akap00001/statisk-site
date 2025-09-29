2f376f5c823ccf96a345244f0ee79a37427b6eca
Akua <akap0001@stud.kea.dk>
2025-09-29 21:03:28 +0200

    prøver at fixe css

--- list.js
+++ list.js
@@ -11,21 +11,22 @@ fetch(`https://kea-alt-del.dk/t7/api/products?limit=20`)
     products.forEach(element => {
         console.log(element);
 
-          ListContainer.innerHTML += `<div class="produkter ${element.soldout && "udsolgt"} ${element.discount && "tilbud"}">
+          ListContainer.innerHTML += `
+        <article class="produkter ${element.soldout && "udsolgt"} ${element.discount && "tilbud"}">
           <div class="små_billeder">
             <a href="produkt.html"> <img src="https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp" alt="Sort Puma skrift rygsæk" /></a>
           </div>
           <h4>${element.productdisplayname}</h4>
-          <div class="små_tekst">
+            <div class="små_tekst">
             <p class="klasse">${element.articletype} | ${element.brandname}</p>
             <p class="pris">DKK ${element.price},-</p>
             <div class="discounted">
-                <p>Now DKK <span>${Math.round(element.price*element.discount/100)}</span>,-</p>
+                <p class="pris_nu">Now DKK <span>${Math.round(element.price*element.discount/100)}</span>,-</p>
                 <p><span>${element.discount}</span>%</p>
             </div>
             <a href="produkt.html?id=${element.id}" class="">Read More</a>
             </div>
-        </div>`;
+        </article>`;
     })
     
     }
