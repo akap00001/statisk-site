0a04aaf3de250cdd1bf53b4659de4b85a85241f1
Akua <akap0001@stud.kea.dk>
2025-09-30 14:40:16 +0200

    laver gender filtrering

--- list.js
+++ list.js
@@ -1,21 +1,43 @@
+const productlistContainer = document.querySelector(".listContainer");
 const params = new URLSearchParams(window.location.search);
 const category = params.get("category");
 //console.log(category);
 
 document.querySelector("h2").textContent = category;
 
+document.querySelectorAll("#filters button")
+.forEach((knap) => knap.addEventListener("click", showFiltered));
+
+function showFiltered() {
+console.log(this.dataset.gender);
+  const gender = this.dataset.gender;
+  if (gender == "All"){
+    showProducts(allData);
+} else {
+  const udsnit = allData.filter((product) => product.gender == gender);
+  showProducts(udsnit);
+}
+}
+
+let allData;
+
 const ListContainer = document.querySelector("main");
 
-fetch(`https://kea-alt-del.dk/t7/api/products?limit=20&category=${category}`)
+fetch(`https://kea-alt-del.dk/t7/api/products?limit=30&category=${category}`)
   .then((response) => response.json())
-  .then(data => showProducts(data));
+  .then(data => {
+    allData = data;
+    showProducts(allData);
+  });
   
     function showProducts(products) {
-    //console.log(products);
-    products.forEach(element => {
+    console.log(products);
+    productlistContainer.innerHTML = "";
+    products.forEach((element) => {
         console.log(element);
 
-          ListContainer.innerHTML += `
+
+    productlistContainer.innerHTML += `
         <article class="produkter ${element.soldout && "udsolgt"} ${element.discount && "tilbud"}">
           <div class="små_billeder">
              <img src="https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp" alt="Sort Puma skrift rygsæk" />
